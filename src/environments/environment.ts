0c000dc9da954556f43bb5f0d4e297050d52b1c8
keshavladha <keshavladha@users.noreply.github.com>
2026-06-01 18:55:35 +0530

    chore: configure EmailJS serviceId in environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6dG93aGpub214Y2ZhbWlodG5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxNDYzMzUsImV4cCI6MjA5NTcyMjMzNX0.dZ1Te3JWeevyznxjmmN4I-D0iEkHeVuIvPLBqlrewow',
   emailjs: {
     publicKey: 'SC6kfyCDG_8yx2opa',
-    serviceId: 'YOUR_EMAILJS_SERVICE_ID',
+    serviceId: 'service_x76l39t',
     contactTemplateId: 'YOUR_EMAILJS_CONTACT_TEMPLATE_ID',
     paymentTemplateId: 'YOUR_EMAILJS_PAYMENT_TEMPLATE_ID',
     adminEmail: 'info@futureinstitute.edu'
