7a23c1c1c3d39f0adfddb5a36c6d9da6d1d3c7bf
keshavladha <keshavladha@users.noreply.github.com>
2026-06-01 18:52:52 +0530

    chore: configure EmailJS public key in environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   supabaseUrl: 'https://jztowhjnomxcfamihtnp.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6dG93aGpub214Y2ZhbWlodG5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxNDYzMzUsImV4cCI6MjA5NTcyMjMzNX0.dZ1Te3JWeevyznxjmmN4I-D0iEkHeVuIvPLBqlrewow',
   emailjs: {
-    publicKey: 'YOUR_EMAILJS_PUBLIC_KEY',
+    publicKey: 'SC6kfyCDG_8yx2opa',
     serviceId: 'YOUR_EMAILJS_SERVICE_ID',
     contactTemplateId: 'YOUR_EMAILJS_CONTACT_TEMPLATE_ID',
     paymentTemplateId: 'YOUR_EMAILJS_PAYMENT_TEMPLATE_ID',
