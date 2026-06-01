f2ef9bd7a2e299fe424e9171e4ce36b50edf5bb0
keshavladha <keshavladha@users.noreply.github.com>
2026-06-01 18:56:22 +0530

    chore: update EmailJS serviceId to active Gmail service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6dG93aGpub214Y2ZhbWlodG5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxNDYzMzUsImV4cCI6MjA5NTcyMjMzNX0.dZ1Te3JWeevyznxjmmN4I-D0iEkHeVuIvPLBqlrewow',
   emailjs: {
     publicKey: 'SC6kfyCDG_8yx2opa',
-    serviceId: 'service_x76l39t',
+    serviceId: 'service_g90nadi',
     contactTemplateId: 'YOUR_EMAILJS_CONTACT_TEMPLATE_ID',
     paymentTemplateId: 'YOUR_EMAILJS_PAYMENT_TEMPLATE_ID',
     adminEmail: 'info@futureinstitute.edu'
